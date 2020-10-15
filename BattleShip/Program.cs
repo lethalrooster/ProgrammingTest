49da6dd1bdb360dfb32d06005aedbf3bbf2a2177
LethalRooster <jonas.stroberg@helsingborg.se>
2020-10-15 12:34:31 +0200

    BattleShipProject

--- BattleShip/Program.cs
+++ BattleShip/Program.cs
@@ -4,13 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ProgrammingTest
+namespace BattleShip
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int age = int.Parse(Console.ReadLine());
         }
     }
 }
